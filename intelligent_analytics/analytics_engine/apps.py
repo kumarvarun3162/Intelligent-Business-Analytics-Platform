0ba6e8a63d47bbf2e9468ec6650c6737985f5a5b
kumarvarun3162 <kumarvarun3162@gmail.com>
2026-05-17 16:45:44 +0530

    restart

--- intelligent_analytics/analytics_engine/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class AnalyticsEngineConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'analytics_engine'
--- intelligent_analytics/analytics_engine/data_processing/exporter.py
+++
@@ -1,2 +0,0 @@
-def export_for_powerbi(df):
-    df.to_csv("powerbi_dataset.csv", index=False)
